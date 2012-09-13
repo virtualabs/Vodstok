cd9ae5e0d0b04d857686561935bf8793ca3d2aaf
virtualabs <virtualabs@gmail.com>
2012-09-14 01:43:57 +0200

    First version of diagnostic page, should be integrated to our installer

---
+++ www/diag.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('utils.php');
+
+echo('Testing chunk dir ... ');
+if (!test_write(CHUNK_DIR))
+    echo('KO<br/>');
+else
+    echo('OK<br/>');
+    
+echo('Testing endpoint dir ... ');
+if (!test_write(ENDPOINT_DIR))
+    echo('KO<br/>');
+else
+    echo('OK<br/>');
