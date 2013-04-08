1bcfbf8bb0a3e912258a1a2d4fe2081385147e31
virtualabs <virtualabs@gmail.com>
2013-04-08 22:58:07 +0200

    Code refactoring (remove extra spaces)

--- client/core/settings.py
+++ client/core/settings.py
@@ -12,12 +12,12 @@ class Settings:
     """
     Vodstok global settings
     """
-    
+
     chunk_size = 32*1024 - 16
     block_size = 16
     max_threads = 5
     version = '1.2.4'
-    
+
     def __init__(self):
         pass
 
