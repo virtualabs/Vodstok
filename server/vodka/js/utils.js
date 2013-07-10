6964d0e82f82aa1e9893c113595f39d2182236a0
virtualabs <virtualabs@gmail.com>
2013-07-10 09:52:34 +0200

    Moved utilities function to utils.js

---
+++ server/vodka/js/utils.js
@@ -0,0 +1,66 @@
+/**
+ * Utils
+ */
+
+/**
+ * Pick a random item from  a list of items
+ *
+ * @param {array} items Item array
+ * @return {object} Item picked
+ */
+var choice = function(items) {
+    return items[Math.floor(Math.random()*items.length)];
+};
+
+
+/**
+ * Convert an hex string to ascii
+ *
+ * @param {string} hex Hex string
+ * @return {string} Ascii string
+ */
+function hex2a(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+/**
+ * Convert an ascii string to hex form
+ *
+ * @param {string} str String to convert
+ * @return {string} Hex string
+ */
+function toHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+/**
+ * Shuffle an array
+ */
+function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+};
+
+/**
+ * Convert a simple array into an indexed-array
+ */
+function makeChunkArray(array){
+    var output = [];
+    /* copy content */
+    for (var i in array) {
+        output.push({
+            id: i,
+            object: array[i]
+        });
+    }
+
+    /* Shuffle output */
+    return shuffle(output);
+};
