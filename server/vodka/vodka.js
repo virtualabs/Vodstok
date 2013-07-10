abd1ff21efab2413f42db3eeb3338595d6ba31e2
virtualabs <virtualabs@gmail.com>
2013-07-10 09:53:05 +0200

    Improved the way a file is downloaded, and fix a bug related to large files (using metadata).

--- server/vodka/vodka.js
+++ server/vodka/vodka.js
@@ -1,72 +1,83 @@
-var choice = function(items) {
-    return items[Math.floor(Math.random()*items.length)];
-};
-
-function hex2a(hex) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
-function toHex(str) {
-    var hex = '';
-    for(var i=0;i<str.length;i++) {
-        hex += ''+str.charCodeAt(i).toString(16);
-    }
-    return hex;
-}
 /**
- * Crypto layer
+ * Vodstok Simple downloader
+ *
+ * @param [url] url Optional URL.
  */
-
 var VodkaSimpleDl = function(url) {
+    /* Parse the requested url, or the current location
+     * if url is not provided
+     */
     if (url != null) {
-        /* Parse url */
         var uri = new Uri(url);
         var ep_uri = uri.protocol()+'://'+uri.host()+':'+uri.port()+uri.path();
+        var chunk_infos = uri.anchor();
     } else {
-        uri = document.location.fragment;
+        chunk_infos = document.location.hash.slice(1);
+        var ep_uri = document.location.protocol+'//';
+        ep_uri += document.location.host;
+        ep_uri += document.location.pathname;
     }
-    
-    var chunk_infos = uri.anchor();
-    var chunk_id = chunk_infos.split('-')[1];
-    var key = chunk_infos.split('-')[0];
 
-    /* Get the first chunk */
-    var chunk = this.dlFile(ep_uri+'?'+chunk_id, key).done(function(content){
-        console.log('content length: '+content[1].length)
-        var len = content[1].length;
-        var buf = new ArrayBuffer(content[1].length);
-        var view = new Uint8Array(buf);
-        for (var i = 0; i < len; i++) {
-              view[i] = content[1].charCodeAt(i) & 0xff;
-        }
-        makeDl(content[0], view);
-    });
-    
+    /* Check chunk sanity */
+    var chunk_patt =  /^[0-9a-f]{32}-[0-9a-f]{32}$/i;
+    if (chunk_patt.test(chunk_infos)) {
+        var chunk_id = chunk_infos.split('-')[1];
+        var key = chunk_infos.split('-')[0];
+
+        /* Launch the download */
+        var chunk = this.dlFile(ep_uri+'?'+chunk_id, key).done(function(content){
+            var len = content[1].length;
+            var buf = new ArrayBuffer(content[1].length);
+            var view = new Uint8Array(buf);
+            for (var i = 0; i < len; i++) {
+                  view[i] = content[1].charCodeAt(i) & 0xff;
+            }
+            makeDl(content[0], view);
+        });
+    } else {
+        console.log('No file to download');
+    }
 };
 
+/**
+ * Download a file given the first chunk and key.
+ *
+ * @this {VodkaSimpleDl}
+ * @param {string} chunk First chunk URL
+ * @param {object} key Decryption key
+ */
 VodkaSimpleDl.prototype.dlFile = function(chunk, key) {
     var dfd = $.Deferred();
 
     this.dlChunk([chunk], key, true).done((function(inst, dfd, key){
         return function(blob){
-            console.log(blob);
-            //blob = hex2a(blob);
-            console.log(blob);
             var infos= blob.split('|');
             var filename = infos[0];
             var version = infos[1];
             var chunks = infos[2].split(',');
-            console.log(chunks);
             if (filename != 'metadata') {
+                /* Launch the download of all chunks. */
                 inst.dlChunk(chunks, key, true).done((function(filename, dfd){
                     return function(content){
                         dfd.resolve([filename, content]);
                     };
                 })(filename, dfd));
             } else {
-                dfd.resolve(inst.dlChunk(chunks, key, false));
+                /* Retrieve the complete metadata and launch the download of all chunks. */
+                dfd.resolve(inst.dlChunk(chunks, key, false)).done((function(inst, dfd, key){
+                    return function(content){
+                        var infos= content.split('|');
+                        var filename = infos[0];
+                        var version = infos[1];
+                        var chunks = infos[2].split(',');
+                        /* Launching the dl of all chunks */
+                        inst.dlChunk(chunks, key, trye).done((function(filename, dfd){
+                            return function(content){
+                                dfd.resolve([filename, content]);
+                            };
+                        })(filename, dfd));
+                    };
+                })(this, dfd, key));
             }
         };
     })(this, dfd, key)).fail((function(dfd){
@@ -76,17 +87,32 @@ VodkaSimpleDl.prototype.dlFile = function(chunk, key) {
     return dfd.promise();
 };
 
-VodkaSimpleDl.prototype.dlChunk = function(chunk, key, last) {
+/**
+ * Download a given chunk and optionnaly parses its content.
+ *
+ * @this {VodkaSimpleDl}
+ * @param {array} chunks Array of chunk to download, decrypt and concatenate.
+ * @param {object} key Decryption key
+ * @param {bool} last Return raw content if true.
+ * @return {Deferred} Returns a deferred.
+ */
+VodkaSimpleDl.prototype.dlChunk = function(chunks, key, last) {
     var dfd = $.Deferred();
 
+    /* Is it the last chunk ? */
     if (last == null)
         last = false;
-    console.log(last);
+
+    /* Deferred tasks */
     var deferreds = [];
+    /* Output blob array */
     var blobs = new Array();
-    for (var i in chunk) {
-        var client = new VodClient(chunk[i].split('?')[0]);
-        var cid = chunk[i].split('?')[1];
+
+    /* Loop on every chunk and download it */
+    var chunk_array = makeChunkArray(chunks);
+    for (var i in chunk_array) {
+        var client = new VodClient(chunk_array[i].object.split('?')[0]);
+        var cid = chunk_array[i].object.split('?')[1];
         var dfd_ = $.Deferred();
         client.dlChunk(cid).done((function(dfd, blobs, id, key){
             return function(content){
@@ -94,24 +120,23 @@ VodkaSimpleDl.prototype.dlChunk = function(chunk, key, last) {
                 blobs[id] = content;
                 dfd.resolve();
             };
-        })(dfd_, blobs, i, key)).fail((function(dfd){
+        })(dfd_, blobs, chunk_array[i].id, key)).fail((function(dfd){
             return function(){dfd.reject();};
         })(dfd));
         deferreds.push(dfd_.promise());
     }
+    /* Once done, put all in one and return it */
     $.when.apply($, deferreds).then((function(inst, dfd, blobs, key, last){
         return function(){
             if (last) {
-                console.log(blobs);
+                /* Return content */
                 dfd.resolve(blobs.join(''));
             } else {
-                console.log(blobs);
+                /* Parse content and returns a deferred. */
                 var content = blobs.join('');
-                console.log(content);
                 var infos = content.split('|');
                 var filename = infos[0];
                 var chunks = infos[1].split(',');
-                console.log(chunks);
                 if (filename == 'metadata') {
                     return inst.dlChunk(chunks, key, false);
                 } else {
@@ -128,11 +153,17 @@ VodkaSimpleDl.prototype.dlChunk = function(chunk, key, last) {
     return dfd.promise();
 };
 
+/**
+ * Encryption routine
+ */
 var encryptChunk = function(data, key) {
     var iv = CryptoJS.SHA512(key).slice(0, 16);
     return CryptoJS.AES.encrypt(data, key, { iv: iv});
 };
 
+/**
+ * Decryption routine
+ */
 var decryptChunk = function(data, key) {
     var iv = CryptoJS.enc.Hex.parse(CryptoJS.SHA512(CryptoJS.enc.Hex.parse(key)).toString().slice(0,32));
     return hex2a(CryptoJS.AES.decrypt(data, CryptoJS.enc.Hex.parse(key), { iv: iv}).toString());
@@ -149,6 +180,7 @@ function makeDl(filename, content) {
 
 //var test = new VodkaSimpleDl('http://virtualabs.fr/vodstok/#7f7f3e635b1b2bbf613d677462f8704c-86aec4b638f84315bcb6fad67dc7aed5');
 var test = new VodkaSimpleDl('http://virtualabs.fr/vodstok/#aa99a1b0fcce7f5934c1d6b52164f8ad-9433d26cdfd1606bb09efccb45a8139c');
+//var test = new VodkaSimpleDl();
 /*
 client.endpoints().done(function(ep){
     console.log(ep);
