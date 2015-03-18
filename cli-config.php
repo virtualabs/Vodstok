143f651c8c7c1ce1ef0809eb0b97bd6c137c5c95
Romain E Silva <r.esilva@sysdream.com>
2015-03-18 22:15:54 +0100

    Init dev-ui

--- cli-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-// replace with file to your own project bootstrap
-require_once 'db/bootstrap.php';
-
-return ConsoleRunner::createHelperSet($entityManager);
